99700d009cceaf6369346992c02677f4e46bca06
Bruno Pupp <brunopupp.bp@gmail.com>
2021-09-22 16:26:13 -0300

    atualizacao rota categorias

--- src/controllers/categorias.js
+++ src/controllers/categorias.js
@@ -37,9 +37,9 @@ async function buscarTodasCategorias() {
   });
 
   let dados = hits.hits;
-  let cat=[];
+  let cat = [];
 
-  dados.forEach(categoria=>{
+  dados.forEach(categoria => {
     let idElastic;
     let idSql;
     let nome;
@@ -49,8 +49,8 @@ async function buscarTodasCategorias() {
     idSql = categoria._source.id;
     nome = categoria._source.nome;
     status = categoria._source.status;
-    
-    cat.push({idElastic:idElastic, idSql:idSql, nome:nome, status:status})
+
+    cat.push({ idElastic: idElastic, idSql: idSql, nome: nome, status: status })
 
 
 
@@ -77,7 +77,7 @@ async function atualizarTrue(ids) {
 }
 
 
-async function criarCategoria(idSql, nome, status){
+async function criarCategoria(idSql, nome, status) {
   let body = await client.index({
     index: index,
     body: {
@@ -91,44 +91,61 @@ async function criarCategoria(idSql, nome, status){
   return body
 }
 
-module.exports = {
-  index(req, res) {
-    try {
 
-
-      client.search({
-        index,
-        // body: {
-        //   'query': {
-        //     'bool': {
-        //       "must": [
-        //         {
-        //           'match': {
-        //             'status': true
-        //           }
-        //         }
-        //       ],
-        //       'must_not': [
-        //         {
-        //           'match': {
-        //             'status': false
-        //           }
-        //         }
-        //       ]
-        //     }
-        //   }
-        // }
-      }, function (err, resp, status) {
-        if (err) {
-          return res.json({ status: 'erro', message: err });
-
-        } else {
-          const { hits } = resp;
-          return res.json({ status: 'sucesso', hits });
-
+async function buscarCategoriasAtivas() {
+  let {hits} = await client.search({
+    index,
+    body: {
+      'query': {
+        'bool': {
+          "must": [
+            {
+              'match': {
+                'status': true
+              }
+            }
+          ],
+          'must_not': [
+            {
+              'match': {
+                'status': false
+              }
+            }
+          ]
         }
+      }
+    }
+  })
 
-      })
+  let dados = hits.hits;
+  let cat = [];
+
+  dados.forEach(categoria => {
+    let idElastic;
+    let idSql;
+    let nome;
+    let status;
+
+    idElastic = categoria._id;
+    idSql = categoria._source.id;
+    nome = categoria._source.nome;
+
+    cat.push({ idElastic: idElastic, idSql: idSql, nome: nome})
+
+
+  })
+
+  return cat;
+
+
+}
+
+module.exports = {
+  async index(req, res) {
+    try {
+      let categorias = await buscarCategoriasAtivas();
+      return res.json({status: 'sucesso', categorias});
+
     } catch (e) {
       return res.json({ status: 'erro', message: e.message })
     }
@@ -177,7 +194,7 @@ module.exports = {
     let cats = [];
     let catAtualizar = [];
     let idsAtualizar = [];
-    let categoriaCriar=[];
+    let categoriaCriar = [];
     let ids = [];
     try {
       let hits = await buscarCategorias();
@@ -186,7 +203,7 @@ module.exports = {
         let categoriaHit = hit._source.nome;
         let idCategoriaHit = hit._id;
 
-        cats.push({ id: idCategoriaHit, nome: categoriaHit, status:hit._source.status });
+        cats.push({ id: idCategoriaHit, nome: categoriaHit, status: hit._source.status });
 
       })
 
@@ -196,10 +213,10 @@ module.exports = {
         cats.forEach(cate => {
           if (cate.nome.toUpperCase() === cat.toUpperCase() && cate.status === false) {
             idsAtualizar.push(cate.id)
-          }else if(cate.nome.toUpperCase() === cat.toUpperCase()){
+          } else if (cate.nome.toUpperCase() === cat.toUpperCase()) {
             ids.push(cate.id)
-          }else{
-            categoriaCriar.push({nome: cat, status: true, id: hits.length + 1, timestamp: moment().format()})
+          } else {
+            categoriaCriar.push({ nome: cat, status: true, id: hits.length + 1, timestamp: moment().format() })
           }
         })
       }
@@ -211,14 +228,14 @@ module.exports = {
 
 
 
-      cats.forEach(categoria =>{
+      cats.forEach(categoria => {
 
-          catAtualizar.push(categoria.id)
+        catAtualizar.push(categoria.id)
 
       })
 
       // idsAtualizar = catAtualizar.filter(item => !ids.includes(item));
-      catAtualizar = catAtualizar.filter(item=> !ids.includes(item))
+      catAtualizar = catAtualizar.filter(item => !ids.includes(item))
 
       // console.log(ids);
       //console.log('cats', catAtualizar)
@@ -263,75 +280,75 @@ module.exports = {
 
   },
 
-  async listar(req,res){
+  async listar(req, res) {
     try {
       let categorias = await buscarTodasCategorias();
 
-      return res.json({status: 'sucesso', categorias});
+      return res.json({ status: 'sucesso', categorias });
 
     } catch (e) {
-      return res.json({status: 'erro', message:e.message})
+      return res.json({ status: 'erro', message: e.message })
     }
   },
 
-  async atualizar(req,res){
-    let {categorias} = req.body;
+  async atualizar(req, res) {
+    let { categorias } = req.body;
 
     try {
 
-      categorias.forEach(async categoria=>{
+      categorias.forEach(async categoria => {
         console.log(categoria)
         let id = categoria.idElastic;
         let status = categoria.status;
-        
-        if(!status || status ===null || status === undefined || status ===''){
-          if(status === false){
 
-          }else{
+        if (!status || status === null || status === undefined || status === '') {
+          if (status === false) {
+
+          } else {
             status = true
 
           }
-        }else{
-          if(status !==false){
+        } else {
+          if (status !== false) {
             status = true
           }
         }
         await atualizar(id, status);
       })
 
-      return res.json({status: 'sucesso', message: 'Categorias atualizadas com sucesso'})
+      return res.json({ status: 'sucesso', message: 'Categorias atualizadas com sucesso' })
     } catch (e) {
-      return res.json({status: 'erro', message: e.message})
+      return res.json({ status: 'erro', message: e.message })
     }
   },
-  async criar(req,res){
-    let {idSql, nome, status} = req.body;
-    let message='';
+  async criar(req, res) {
+    let { idSql, nome, status } = req.body;
+    let message = '';
     try {
-      if(!idSql || idSql === null || idSql===undefined || idSql===''){
-        message+= 'Informe o id da categoria no Sql. '
+      if (!idSql || idSql === null || idSql === undefined || idSql === '') {
+        message += 'Informe o id da categoria no Sql. '
       }
-      if(!nome || nome === null || nome === undefined || nome ===''){
+      if (!nome || nome === null || nome === undefined || nome === '') {
         message += 'Informe o nome da categoria. '
       }
 
-      if(message.length >0){
-        return res.json({status: 'erro', message: message})
+      if (message.length > 0) {
+        return res.json({ status: 'erro', message: message })
       }
 
-      if(!status || status === undefined || status === null || status === ''){
-        if(status === false){
+      if (!status || status === undefined || status === null || status === '') {
+        if (status === false) {
           status = false
-        }else{
+        } else {
           status = true
         }
       }
 
       let body = await criarCategoria(idSql, nome, status);
 
-      return res.json({status: 'sucesso', message: body})
+      return res.json({ status: 'sucesso', message: body })
     } catch (e) {
-      return res.json({status: 'erro', message: e.message});
+      return res.json({ status: 'erro', message: e.message });
     }
   }
 
